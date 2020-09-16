7e0d69cddf0c11eee5b8921c19295229549e991e
ueda eri <eri.ueda@di-v.co.jp>
2020-09-16 18:05:45 +0900

    Revert "取り消し"
    This reverts commit f6163296dfb945374da861b3cf3a000ca6eeab79.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,12 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable,  :validatable
 
-  # <<バリデーション>>
-  validates :nickname, presence: true, uniqueness: { case_sensitive: true }
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
 
-  validates :birth_date, presence: true
-
-  # パスワードの英数字混在を否定
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-
-  # 全角のひらがなor漢字を使用していないか検証
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
-    validates :first_name
-    validates :last_name
-  end
-
-  # 全角のカタカナを使用していないか検証
-  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-
-  # <<アソシエーション>>
-  has_many :items
-  has_many :item_transaction
+  validates :name, presence: true
 end
